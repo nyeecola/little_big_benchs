9b1a07fa0a188060045a35ea9958f906cc30fd25
Italo Nicola <italo.niccola@gmail.com>
2022-03-13 05:27:21 -0300

    Display relevant error messages when a test case fails

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include <vector>
 
+#include "vector.h"
 #include "util.h"
-#include "test_util.h"
-#include "test_cases.cpp"
 
-#define __TEST_IMPLEMENTATION__
-#include "test_util.h"
+struct TestCaseResult {
+    bool passed;
+    const char *error_str;
+};
 
-typedef bool (*TestFuncPtr)();
+typedef TestCaseResult (*TestFuncPtr)();
 
 struct TestCase {
     const char *name;
     TestFuncPtr func;
 };
+
+#include "test_util.h"
+#include "test_cases.cpp"
+
+#define __TEST_IMPLEMENTATION__
+#include "test_util.h"
 
 TestCase test_cases[] = {
     #include "test_cases.cpp"
@@ -30,16 +39,19 @@ int main(int argc, char **argv) {
     int num_passes = 0, num_fails = 0;
 
     for (int i = 0; i < num_tests; i++) {
-        bool result = test_cases[i].func();
-        num_passes += result;
-        num_fails += !result;
+        TestCaseResult result = test_cases[i].func();
+        num_passes += result.passed;
+        num_fails += !result.passed;
 
-        fprintf(stderr, "Test #%d (" CYAN("%s") "): %s\n", i+1, test_cases[i].name, result ? PASS : FAIL);
+        fprintf(stderr, "Test #%d (" CYAN("%s") "): %s\n", i+1, test_cases[i].name, result.passed ? PASS : FAIL);
+        if (!result.passed) {
+            fprintf(stderr, "\t-> %s\n", result.error_str);
+        }
     }
     fprintf(stderr, PRINT_BORDER "\n");
     fprintf(stderr, "Finished: %d " GREEN("passed") " %d " RED("failed") "\n", num_passes, num_fails);
     fprintf(stderr, PRINT_BORDER "\n\n");
 
-    return 0;
+    return num_fails ? -1 : 0;
 }
 
--- src/test_util.h
+++ src/test_util.h
@@ -1,15 +1,19 @@
 #ifndef __TEST_IMPLEMENTATION__
 // test declaration (first pass)
 
-#define EXPECT_EQ(a, b) if (!(a == b)) return false;
-#define EXPECT_LT(a, b) if (!(a < b)) return false;
-#define EXPECT_GT(a, b) if (!(a > b)) return false;
+#define STRINGIFY2(a) #a
+#define STRINGIFY(a) STRINGIFY2(a)
+#define LINE_AS_STR STRINGIFY(__LINE__)
+
+#define EXPECT_EQ(a, b) if (!(a == b)) return { false, __FILE__ ":" LINE_AS_STR ": EXPECT_EQ(" #a", " #b") failed" };
+#define EXPECT_LT(a, b) if (!(a < b)) return { false, __FILE__ ":" LINE_AS_STR ": EXPECT_LT(" #a", " #b") failed" };
+#define EXPECT_GT(a, b) if (!(a > b)) return { false, __FILE__ ":" LINE_AS_STR ": EXPECT_GT(" #a", " #b") failed" };
 
 #undef TEST_CASE
 #define TEST_CASE(test_name, ...) \
-bool test_ ## test_name () { \
+TestCaseResult test_ ## test_name () { \
     __VA_ARGS__ \
-    return true; \
+    return { true, "" }; \
 }
 
 #else
